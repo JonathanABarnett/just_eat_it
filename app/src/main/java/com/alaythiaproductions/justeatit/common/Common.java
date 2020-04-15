97ceeff0fd52c18d4495b801ee6780f526390f08
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-04-15 11:53:15 -0500

    Add phone registration

---
+++ app/src/main/java/com/alaythiaproductions/justeatit/common/Common.java
@@ -0,0 +1,8 @@
+package com.alaythiaproductions.justeatit.common;
+
+import com.alaythiaproductions.justeatit.model.UserModel;
+
+public class Common {
+    public static final String USER_REFERENCES = "Users";
+    public static UserModel currentUser;
+}
