8f3dc96b635286294c9fd380cd6d4396f8e8b955
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-04-15 14:07:57 -0500

    Add HomeActivity with Navigation Drawers. Add Popular Categories and Best Deal models.

---
+++ app/src/main/java/com/alaythiaproductions/justeatit/model/BestDealModel.java
@@ -0,0 +1,47 @@
+package com.alaythiaproductions.justeatit.model;
+
+public class BestDealModel {
+
+    private String menuId, foodId, name, image;
+
+    public BestDealModel() {}
+
+    public BestDealModel(String menuId, String foodId, String name, String image) {
+        this.menuId = menuId;
+        this.foodId = foodId;
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(String menuId) {
+        this.menuId = menuId;
+    }
+
+    public String getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(String foodId) {
+        this.foodId = foodId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
